33262766de3cd78ea047760843ff21296b70deb2
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2022-04-17 22:04:21 +0200

    fix redux operactions to work without api

--- src/ducks/Movies/operations.js
+++ src/ducks/Movies/operations.js
@@ -1,74 +1,27 @@
 import axios from "axios";
 import * as actions from './actions';
-
-export const checkConnetion = () => {
-    return async () => {
-        try {
-            const response = await 
-            axios.get('http://localhost:5000/api/movies');
-            if(response.status === 200){
-                return "OK"
-            }
-        } catch(error) {
-            console.log(error);
-            return "NOT OK"
-        }
-    }
-}
+import movies from './../movies.json';
 
 export const getMovies = () => {
-    return async dispatch => {
-        await axios.get('http://localhost:5000/api/movies').then((response) => {
-            if (response.status === 200){
-                dispatch(actions.getMoviesAction(response.data));
-            }
-        }).catch((error)=>{
-            console.log(error)
-        });
+    return dispatch => {
+        dispatch(actions.getMoviesAction(movies));
     }
 }
 
 export const addMovie = (newMovie) => {
     return async dispatch => {
-        try {
-            const response = await 
-            axios.post('http://localhost:5000/api/movies', newMovie);
-            if(response.status === 200){
-                dispatch(actions.addMovieAction(response.data));
-                return "OK"
-            }
-        } catch(error) {
-            console.log(error);
-            return "NOT OK"
-        }
+        dispatch(actions.addMovieAction(newMovie));
     }
 }
 
 export const updateMovie = (movie) => {
     return async dispatch => {
-        try {
-            const response = await 
-            axios.put(`http://localhost:5000/api/movies/${movie.id}`, movie);
-            if(response.status === 200){
-                dispatch(actions.updateMovieAction(response.data));
-                return "OK"
-            }
-        } catch(error) {
-            console.log(error);
-            return "NOT OK"
-        }
+        dispatch(actions.updateMovieAction(movie));
     }
 }
 
 export const deleteMovie = (movieId) => {
     return async dispatch => {
-        await axios.delete(`http://localhost:5000/api/movies/${movieId}`).then((response) => {
-            if (response.status === 200){
-                dispatch(actions.deleteMovieAction({id: movieId}));
-                return response
-            }
-        }).catch((error)=>{
-            console.log(error)
-        })
+        dispatch(actions.deleteMovieAction({id: movieId}));
     }
 }
--- src/ducks/Persons/operations.js
+++ src/ducks/Persons/operations.js
@@ -1,71 +1,29 @@
 import axios from "axios";
 import * as actions from './actions';
 
-export const checkConnetion = () => {
-    return async () => {
-        try {
-            const response = await 
-            axios.get('http://localhost:5000/api/persons');
-            if(response.status === 200){
-                return "OK"
-            }
-        } catch(error) {
-            console.log(error);
-            return "NOT OK"
-        }
-    }
-}
+import persons from './../persons.json';
 
 export const getPersons = () => {
-    return async dispatch => {
-        await axios.get('http://localhost:5000/api/persons').then((response) => {
-            dispatch(actions.getPersonsAction(response.data));
-        }).catch((error)=>{
-            console.log(error)
-        });
+    return dispatch => {
+        dispatch(actions.getPersonsAction(persons));
     }
 }
 
 
 export const addPerson = (newPerson) => {
     return async dispatch => {
-        try {
-            const response = await 
-            axios.post('http://localhost:5000/api/persons', newPerson);
-            if(response.status === 200){
-                dispatch(actions.addPersonAction(response.data));
-                return "OK"
-            }
-        } catch(error) {
-            console.log(error);
-            return "NOT OK"
-        }
+        dispatch(actions.addPersonAction(newPerson));
     }
 }
 
 export const updatePerson = (person) => {
     return async dispatch => {
-        try {
-            const response = await 
-            axios.put(`http://localhost:5000/api/persons/${person.id}`, person);
-            if(response.status === 200){
-                dispatch(actions.updatePersonAction(response.data));
-                return "OK"
-            }
-        } catch(error) {
-            console.log(error);
-            return "NOT OK"
-        }
+        dispatch(actions.updatePersonAction(person));
     }
 }
 
 export const deletePerson = (PersonId) => {
     return async dispatch => {
-        await axios.delete(`http://localhost:5000/api/persons/${PersonId}`).then((response) => {
-            dispatch(actions.deletePersonAction({id: PersonId}));
-            return response
-        }).catch((error)=>{
-            console.log(error)
-        })
+        dispatch(actions.deletePersonAction({id: PersonId}));
     }
 }
